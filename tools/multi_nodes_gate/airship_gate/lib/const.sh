8e9da29988ddf9266ff72f473585d63ef4552ef5
Alexander Noskov <anoskov@mirantis.com>
2020-02-07 11:50:21 -0600

    Move deployment scripts to treasuremap
    Change-Id: Ie516b6f9f608198e904fa33227c12c55181a3561

--- tools/multi_nodes_gate/airship_gate/lib/const.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export GENESIS_NAME=n0
-export BUILD_NAME=build
-export SSH_CONFIG_DIR=${WORKSPACE}/multi_nodes_gate/airship_gate/config-ssh
-export TEMPLATE_DIR=${WORKSPACE}/multi_nodes_gate/airship_gate/templates
-export XML_DIR=${WORKSPACE}/multi_nodes_gate/airship_gate/xml
